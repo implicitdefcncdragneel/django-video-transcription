50f6bccafb8a2e6c5e2e241a392f632367b7897b
Chandra Nandan <chandranandan.chandrakar@gmail.com>
2023-04-25 18:51:28 +0530

    added:speech-transcript-view

--- video_transcription_app/views.py
+++ video_transcription_app/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+import pytube
+import subprocess
+from pydub import AudioSegment
+import speech_recognition as speech
 
-# Create your views here.
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from video_transcription_app.models import Video
+
+@csrf_exempt
+def transcribe_video(request):
+    if request.method == 'POST':
+
+        video_url = request.POST.get('video_url')
+
+        video_file = 'video.mp4'
+        audio_file = 'audio.wav'
+        
+        with open(audio_file, 'w') as file:
+            ...
+
+        video_date = pytube.YouTube(video_url)
+        stream = video_date.streams.get_highest_resolution()
+        stream.download(filename="video.mp4")
+
+        subprocess.run(['ffmpeg', '-i', video_file, '-f', 'wav', audio_file])
+        
+        audio = AudioSegment.from_wav(audio_file)
+
+        rec = speech.Recognizer()
+        with speech.AudioFile(audio_file) as source:
+            audio = rec.record(source)
+        transcript = rec.recognize_google(audio)
+
+        video = Video.objects.create(video_url=video_url, transcript=transcript)
+        video.save()
+
+        return JsonResponse({'message': transcript})
