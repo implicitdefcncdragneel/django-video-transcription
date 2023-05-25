6789d2ddae8c12e169121a3badb0dc913cc353a9
Chandra Nandan <chandranandan.chandrakar@gmail.com>
2023-05-25 23:43:02 +0530

    update:template

--- video_transcription_app/views.py
+++ video_transcription_app/views.py
@@ -6,25 +6,24 @@ import speech_recognition as speech
 
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-
 from video_transcription_app.models import Video
 
 @csrf_exempt
 def transcribe_video(request):
-    try:
-        if request.method == 'POST':
-
+    if request.method == 'POST':
+        message = ""
+        try:
             video_url = request.POST.get('video_url')
 
             video_file = 'video.mp4'
             audio_file = 'audio.wav'
-            
-            video_date = pytube.YouTube(video_url)
-            stream = video_date.streams.get_highest_resolution()
+
+            video_data = pytube.YouTube(video_url)
+            stream = video_data.streams.get_highest_resolution()
             stream.download(filename="video.mp4")
 
             subprocess.run(['ffmpeg', '-i', video_file, '-f', 'wav', audio_file])
-            
+
             audio = AudioSegment.from_wav(audio_file)
 
             rec = speech.Recognizer()
@@ -34,13 +33,22 @@ def transcribe_video(request):
 
             video = Video.objects.create(video_url=video_url, transcript=transcript)
             video.save()
-
+            message = transcript
             os.remove(video_file)
             os.remove(audio_file)
-
-            return render(request, 'transcription_result.html', {'message': transcript})
-    except KeyError:
-        transcribe_video(request)
-    except Exception as e:
-        return render(request, 'transcription_result.html', {'message': "Please Reload The Pages"})
+        except pytube.exceptions.PytubeError as e:
+            message = "An error occurred while downloading the video: {}".format(str(e))
+        except subprocess.CalledProcessError as e:
+            message = "An error occurred while converting the video to audio: {}".format(str(e))
+        except speech.UnknownValueError:
+            message = "Error: Speech Recognition could not understand audio"
+        except speech.RequestError as e:
+            message = "Error: Could not request results from Speech Recognition service: {}".format(str(e))
+        except Exception as e:
+            # message = "An unexpected error occurred: {}".format(str(e))
+            transcribe_video(request)
+        context = {
+            'message': message
+        }
+        return render(request, 'transcription_result.html', context)
     return render(request, 'transcribe_video.html')
